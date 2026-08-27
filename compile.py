36fec972874f50805a69eed5c1fcd5541acfdc78
python-nerd1235 <calboyg93@gmail.com>
2026-08-27 19:26:15 -0400

    Implement BF to unary conversion function
    Refactor BF program compilation logic and add input handling.

--- compile.py
+++ compile.py
@@ -1,9 +1,14 @@
-from ONL import ONL
-o=None
-fine=input('>')
-with open(fine+'.bf','r') as f:
-    o=ONL.compile(f.read())
-    print(f'hex: {o}\nnumber: {int(o, 16)}')
-with open(fine+'.bfhex','w') as f:
-    f.write(str(o))
-    print('Done.')
+#to convert a BF program to uanary
+def compile(code):
+        o='0x'
+        for i in code:
+            if i=='+':o+='8'
+            elif i=='-':o+='9'
+            elif i=='.':o+='a'
+            elif i==',':o+='b'
+            elif i=='>':o+='c'
+            elif i=='<':o+='d'
+            elif i=='[':o+='e'
+            elif i==']':o+='f'
+        return o
+print(f"number of zeros:{int(compile(input()),16)}")
